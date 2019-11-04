38fd5899dfdb104f70de90b984c2ca4a9d823200
GerardClotet <36155703+GerardClotet@users.noreply.github.com>
2019-11-04 21:41:53 +0100

    Swap page part 1

---
+++ routes/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(RutasApp());
+}
+
+class RutasApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.lightBlue),
+      home: FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Primera Pantalla'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('open second screen'),
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute( //go to the next pagefounded
+              builder: (context) => SecondScreen(), //set the buildcontext to the following screen
+            ));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Segunda Pantalla'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Back'),
+          onPressed: (){
+            Navigator.of(context).pop(); //return to the previus buildcontext page
+          },
+        ),
+      ),
+    );
+  }
+}
