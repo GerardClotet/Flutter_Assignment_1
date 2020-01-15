280939e07b60011bd950f89e38f2feb6aea3e2e2
GerardClotet <36155703+GerardClotet@users.noreply.github.com>
2020-01-15 20:10:11 +0100

    conecting firebase

---
+++ firebase/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+void main() => runApp(FireBaseApp());
+
+class FireBaseApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Firebase')),
+        body: StreamBuilder(
+          stream: Firestore.instance.collection('tareas').snapshots(),
+          builder: (context,AsyncSnapshot<QuerySnapshot>snapshot){
+            if(!snapshot.hasData)
+            {
+              return Center(child: CircularProgressIndicator(),);
+            }
+
+            List<DocumentSnapshot> docs = snapshot.data.documents;
+            return ListView.builder(
+              itemCount: docs.length,
+              itemBuilder: (context,index){
+                Map<String,dynamic> data = docs[index].data;
+                return ListTile(
+                  leading: Checkbox(value: data['done']),
+                  title: Text(data['what']),
+                );
+              },
+            );
+          }
+        ),
+      ),
+    );
+  }
+}
